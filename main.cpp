0738e9bb327361e62cdf6f46c66a7ba34fa5eb89
Vladimir <barashek.singer@gmail.com>
2026-09-22 20:07:47 +0300

    Second good version
    +Dynamic memory
    +giant text

--- main.cpp
+++ main.cpp
@@ -8,13 +8,22 @@ struct TextLine{
     char *start, *end;
 };
 
-const size_t MAX_STRINGS_COUNT = 20;
-const size_t MAX_SYMBOLS_COUNT = 50;
+struct Text{
+    char* buf;
+    size_t size;
+    size_t lines_count;
+    TextLine* lines;
+};
+
+// const size_t MAX_STRINGS_COUNT = 7000;
+// const size_t MAX_SYMBOLS_COUNT = 100;
 const char* INPUT_TEXT_FILE = "beautiful_text.txt";
 const char* OUTPUT_TEXT_FILE = "mytexts.txt";
 
-int PrintTextToFile(const TextLine* text_lines, size_t nstrings, const char* name_file, const char* access_mode);
-int InputTextFromFile(char text[MAX_STRINGS_COUNT][MAX_SYMBOLS_COUNT], const char* name_file);
+void FreeText(Text* text);
+int SplitText(Text* text);
+int PrintTextToFile(const Text* text, const char* name_file, const char* access_mode);
+int DuplicateTextFromFile(Text* text, const char* name_file);
 int StrBackCmp(const void* ptr_str1, const void* ptr_str2);
 int StrReverseCmp(const void* ptr_str1, const void* ptr_str2);
 int StrForwardCmp(const void* str1, const void* str2);
@@ -25,91 +34,129 @@ void QuickSort(void* data, size_t size_el, size_t left, size_t right, int (*Comp
 void BubbleSort(void* data, size_t size_arr, size_t size_el, int (*CompFunc)(const void* a, const void* b));
 void PrintStringArray(const TextLine* text_lines, size_t size);
 
-int main(){ //после ебаной стирки у меня заработал код, очень многи варнингов и ошибок было 3:00 (реально 3:00)
-    char text[MAX_STRINGS_COUNT][MAX_SYMBOLS_COUNT] = {};
+int main(){
+    Text text = {0};
 
-    int nstrings = InputTextFromFile(text, INPUT_TEXT_FILE);
-
-    if (nstrings < 0){
-        perror("Ну чота с входным файлом");
+    if (DuplicateTextFromFile(&text, INPUT_TEXT_FILE))
         return 0;
-    }
 
-    TextLine text_lines[MAX_STRINGS_COUNT] = {};
+    if (SplitText(&text))
+        return 0;
 
-    for (size_t i = 0; i < (size_t)nstrings; ++i){
-        text_lines[i].end = text_lines[i].start = text[i];
-        while (*text_lines[i].end != '\0')
-            ++text_lines[i].end;
-    }
+    QuickSort(text.lines, sizeof(TextLine), 0, text.lines_count - 1, &StrForwardCmp);
 
-    QuickSort(text_lines, sizeof(text_lines[0]), 0, nstrings - 1, &StrForwardCmp);
+    if (PrintTextToFile(&text, OUTPUT_TEXT_FILE, "w"))
+        return 0;
 
-    if (PrintTextToFile(text_lines, nstrings, OUTPUT_TEXT_FILE, "w") < 0){
-        perror("Ну чота с выходным файлом");
-    }
-    PrintStringArray(text_lines, nstrings);
+    qsort(text.lines, text.lines_count, sizeof(TextLine), &StrReverseCmp);
 
-    printf("----------------------------------\n");
+    if (PrintTextToFile(&text, OUTPUT_TEXT_FILE, "a"))
+        return 0;
 
-    qsort(text_lines, nstrings, sizeof(text_lines[0]), &StrReverseCmp);
+    BubbleSort(text.lines, text.lines_count, sizeof(TextLine), &StrBackCmp);
 
-    if (PrintTextToFile(text_lines, nstrings, OUTPUT_TEXT_FILE, "a") < 0){
-        perror("Ну чота с выходным файлом");
-    }
-    PrintStringArray(text_lines, nstrings);
+    if (PrintTextToFile(&text, OUTPUT_TEXT_FILE, "a"))
+        return 0;
 
-    printf("----------------------------------\n");
-
-    BubbleSort(text_lines, nstrings, sizeof(text_lines[0]), &StrBackCmp);
-
-    if (PrintTextToFile(text_lines, nstrings, OUTPUT_TEXT_FILE, "a") < 0){
-        perror("Ну чота с выходным файлом");
-    }
-    PrintStringArray(text_lines, nstrings);
-}
-
-int PrintTextToFile(const TextLine* text_lines, size_t nstrings, const char* name_file, const char* access_mode){
-    assert(text_lines && name_file);
-
-    FILE* fp = fopen(name_file, access_mode);
-
-    if (fp == NULL)
-        return -1;
-
-    fprintf(fp, "-----------------------------\n");
-
-    for (size_t i = 0; i < nstrings; i++)
-        fprintf(fp, "%s", text_lines[i].start);
-
-    fprintf(fp, "-----------------------------");
-
-    if (fclose(fp) == EOF)
-        return -2;
+    FreeText(&text);
 
     return 0;
 }
 
-int InputTextFromFile(char text[MAX_STRINGS_COUNT][MAX_SYMBOLS_COUNT], const char* name_file){
+void FreeText(Text* text){ //
+    assert(text);
+
+    free(text->lines);
+    free(text->buf);
+}
+
+int SplitText(Text* text){
+    assert(text);
+
+    for (size_t cur_ch = 0; cur_ch < text->size; cur_ch++){
+        if (*(text->buf + cur_ch) == '\n'){
+            ++text->lines_count;
+            *(text->buf + cur_ch) = '\0';
+        }
+    }
+
+    text->lines = (TextLine*)calloc(text->lines_count, sizeof(TextLine));
+    if (text->lines == NULL){
+        printf("Проблемы с динамической памятью");
+        return -3;
+    }
+
+    size_t cur_ch = 0;
+
+    for (size_t i = 0; i < text->lines_count; i++){
+        text->lines[i].start = text->buf + cur_ch;
+        while (*(text->buf + cur_ch) != '\0')
+            ++cur_ch;
+
+        text->lines[i].end = text->buf + cur_ch;
+        ++cur_ch;
+    }
+
+    return 0;
+}
+
+int PrintTextToFile(const Text* text, const char* name_file, const char* access_mode){
+    assert(text && name_file && access_mode);
+
+    FILE* fp = fopen(name_file, access_mode);
+
+    if (fp == NULL){
+        perror("Проблема с открытием файла");
+        return -1;
+    }
+
+    fprintf(fp, "-----------------------------\n");
+
+    // printf()
+
+    for (size_t i = 0; i < text->lines_count; i++)
+        fprintf(fp, "%s\n", text->lines[i].start);
+
+    fprintf(fp, "-----------------------------\n");
+
+    if (fclose(fp) == EOF){
+        perror("Проблема с закрытием файла");
+        return -2;
+    }
+
+    return 0;
+}
+
+int DuplicateTextFromFile(Text* text, const char* name_file){ //
     assert(text && name_file);
 
     FILE* fp = fopen(name_file, "r");
-
-    if (fp == NULL)
+    if (fp == NULL){
+        perror("Проблема при открытии файла");
         return -1;
+    }
 
-    size_t nstrings = 0;
+    fseek(fp, 0, SEEK_END);
+    text->size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
 
-    while (fgets(text[nstrings], MAX_SYMBOLS_COUNT, fp) != NULL)
-        ++nstrings;
+    text->buf = (char*)calloc(text->size, sizeof(char));
+    if (text->buf == NULL){
+        if (fclose(fp) == EOF){
+            perror("Проблема с закрытием файла");
+            return -2;
+        }
 
-    if (fclose(fp) == EOF)
-        return -2;
+        printf("Проблемы с динамической памятью");
+        return -3;
+    }
 
-    return (int)nstrings;
+    fread(text->buf, 1, text->size, fp);
+
+    return 0;
 }
 
-int StrBackCmp(const void* ptr_str1, const void* ptr_str2){ //0:26 гуд партия довольна (0:57 x2)
+int StrBackCmp(const void* ptr_str1, const void* ptr_str2){
     assert(ptr_str1 && ptr_str2);
 
     if (size_t(((const TextLine*)ptr_str1)->start) < size_t(((const TextLine*)ptr_str2)->start))
@@ -121,7 +168,7 @@ int StrBackCmp(const void* ptr_str1, const void* ptr_str2){ //0:26 гуд партия до
     return 0;
 }
 
-int StrReverseCmp(const void* ptr_text_line1, const void* ptr_text_line2){ //0:26 тут полнейший пиздец | 1:03 переделал на структуру, вроде норм
+int StrReverseCmp(const void* ptr_text_line1, const void* ptr_text_line2){
     assert(ptr_text_line1 && ptr_text_line2);
 
     const char* const ptr_str1 = ((const TextLine*)ptr_text_line1)->start;
@@ -155,7 +202,7 @@ int StrReverseCmp(const void* ptr_text_line1, const void* ptr_text_line2){ //0:2
     return 0;
 }
 
-int StrForwardCmp(const void* ptr_text_line1, const void* ptr_text_line2){ //1:05 переделал на структуры
+int StrForwardCmp(const void* ptr_text_line1, const void* ptr_text_line2){
     assert(ptr_text_line1 && ptr_text_line2);
 
     const char* str1 = ((const TextLine*)ptr_text_line1)->start;
@@ -186,7 +233,7 @@ void* GetPtr(const void* data, size_t size_el, size_t ind){
     return (void*)((size_t)data + ind * size_el);
 }
 
-void Swap(void* a, void* b, size_t size_el){ //TODO opt
+void Swap(void* a, void* b, size_t size_el){
     assert(a && b);
 
     char* ptr_a = (char*)a;
